54ac09eaf5942ab60fbd0d24b750c9e17a75e79a
Phan Mạnh Phú <79186630+phanmanhphu1402@users.noreply.github.com>
2023-05-16 08:01:14 +0700

    Delete bansach directory

--- bansach/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "localhost";
-$user = "root";
-$pass ="";
-$database = "dataonline";
-
-$conn = mysqli_connect($host, $user, $pass, $database);
-mysqli_set_charset($conn, "utf8");
-?>
